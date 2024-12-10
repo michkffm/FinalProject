f9942e91cc1812bad72076b81e630829d71b4c80
suekrue <suekrue.cebeci@dci-student.org>
2024-12-10 11:10:50 +0100

    register

--- backend/sever.js
+++ backend/sever.js
@@ -1,9 +1,41 @@
 import express from "express";
 import mongoose from "mongoose";
+import User from './models/User.js'
+import cors from 'cors';
+
+await mongoose.connect(process.env.DB_URI);
 
 const app = express();
-const PORT = process.env.PORT
+const port = process.env.PORT
+app.use(cors())
 
-use.app(express.json());
+app.use(express.json());
 
-app.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`));
+app.post("/register", async (req, res) => {
+    const { username, email, password, role } = req.body;
+  
+    if (!email || !password || !username || !role) {
+      return res.status(400).json({error: 'Invalid registration'});
+    }
+  
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+    //   const verificationToken = crypto.randomBytes(32).toString("hex");
+      
+  
+      const user = await User.create({
+        username,
+        email,
+        password: hashedPassword,
+        role
+      })
+  
+  
+      res.status(201).json(user);
+    } catch (error) {
+      res.status(500).json({error: error.message})
+    }
+    
+  })
+
+app.listen(port, () => console.log(`Server läuft auf Port ${port}`));
