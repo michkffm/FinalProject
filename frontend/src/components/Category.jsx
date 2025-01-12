49af583107813415f78bccc7a88ac676af152738
Pacome Adoufack <tsamoadoufack@yahoo.com>
2025-01-12 05:18:05 +0100

    Beschreibung Ihrer Änderungen

--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -173,7 +173,7 @@ export function Category() {
 
         {/* Gefilterte Jobs anzeigen */}
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-20">
           {filteredData.map((job) => (
             <div
               key={job._id}
