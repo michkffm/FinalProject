00c947284e4a080dea381302b1833edd647294c0
Okyanuspol <ssonercebeci93@gmail.com>
2025-01-23 09:30:40 +0100

    Achtung

--- backend/server.js
+++ backend/server.js
@@ -30,8 +30,8 @@ app.post("/register", async (req, res) => {
     return res.status(400).json({ error: "Invalid registration" });
   }
 
-  if (password.length < 8) {
-    return res.status(400).json({ error: "Password must be at least 8 characters long" });
+  if (password.length <= 8) {
+    return res.status(400).json({ error: "Password muss mindesten 8 Zeichen lang sein." });
   }
 
   try {
