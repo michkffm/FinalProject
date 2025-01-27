062c167b8bb2bf3fccc15d0938f3bd7799cc52d3
faik.bagci@hotmail.de <faik.bagci@hotmail.de>
2025-01-27 10:52:53 +0100

    style Slide

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -55,15 +55,15 @@ export function Home() {
 
   return (
     <main className="zero-section flex flex-col items-center sm:pt-28 sm:pb-40 pb-10 sm:gap-20 gap-0 mt-[-50px]">
-      <div className="handshake-section absolute inset-0 z-5">
+      <div className="handshake-section absolute inset-0 z-1">
         {message && (
-          <p className="flex justify-center text-red-500 text-2xl mt-20 z-10">
+          <p className="flex justify-center text-red-500 text-2xl mt-20 z-15">
             {message}
           </p>
         )}
       </div>
       <section className="flex flex-col justify-center items-center max-w-5xl sm:mt-9 mt-20 p-8 text-center">
-        <h1 className="bg-white bg-opacity-0 pl-2 pr-2 rounded text-xl sm:text-4xl text-teal-500 font-bold">
+        <h1 className="bg-white bg-opacity-0 pl-2 pr-2 rounded text-xl sm:text-4xl text-teal-500 font-bold z-10">
           Finde qualifizierte Fachleute für Deine Aufgaben
         </h1>
         <p className="bg-white bg-opacity-0 pl-2 pr-2 rounded text-gray-600 mt-2 z-10">
