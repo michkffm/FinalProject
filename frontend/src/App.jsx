34a8265b5fb78cd91e4fff5fc4bdfed5ba92d254
unknown <Michael.Koch@vodafone.de>
2025-01-31 11:09:35 +0100

    ProtectedRoute, .env datei update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,6 +24,7 @@ import { ResetPassword } from "./components/ResetPassword.jsx";
 import { Help } from "./components/Help.jsx";
 import { MessagesPage } from "./components/MessagesPage.jsx"; // Importiere die neue Nachrichten-Seite
 import { PaymentMethods } from "./components/PaymentMethods.jsx"; // hinzugefügt von Bilal
+import { ProtectedRoute } from "./components/ProtectedRoute.jsx"; // Importiere die ProtectedRoute-Komponente
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   useEffect(() => {
@@ -38,10 +39,19 @@ function App() {
       <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} /> {/* Header immer sichtbar */}
       <div className="content flex-grow">
         <Routes>
-          {/* Hauptseiten-Routen */}
+          {/* public routes */}
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+          <Route path="/impressum" element={<Impressum />} />
+          <Route path="/passwort-vergessen" element={<ForgotPassword />} />
+          <Route path="/passwort-reset/:token" element={<ResetPassword />} />
+          <Route path="/agb" element={<TermsAndConditions />} /> {/* Route für AGB */}
+          <Route path="/contact" element={<ContactForm />} /> {/* Route für das Kontaktformular */}
+          <Route path="/datenschutz" element={<Datenschutz />} /> {/* Route für die Datenschutzerklärung */}
+          <Route path="/hilfe" element={<Help />} />
+          {/* protected routes */}
+          <Route element={<ProtectedRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/categorie/:name" element={<Category />} />
           <Route path="/payment/:jobId" element={<PaymentMethods />} /> {/* hinzugefügt von Bilal */}
@@ -51,15 +61,9 @@ function App() {
           <Route path="/ratingsView/:jobId" element={<RatingsView/>} />
           <Route path="/suche" element={<Search/>} />
           <Route path="/profile/profileDelete/:id" element={<ProfileDelete />} />
-          <Route path="/impressum" element={<Impressum />} />
-          <Route path="/passwort-vergessen" element={<ForgotPassword />} />
-          <Route path="/passwort-reset/:token" element={<ResetPassword />} />
-          <Route path="/agb" element={<TermsAndConditions />} /> {/* Route für AGB */}
-          <Route path="/contact" element={<ContactForm />} /> {/* Route für das Kontaktformular */}
-          <Route path="/datenschutz" element={<Datenschutz />} /> {/* Route für die Datenschutzerklärung */}
           <Route path="/inbox" element={<Inbox />} />
-          <Route path="/hilfe" element={<Help />} />
           <Route path="/messages" element={<MessagesPage />} /> {/* Neue Route für Nachrichten */}
+          </Route>
         </Routes>
       </div>
       <Footer /> {/* Footer immer sichtbar */}
